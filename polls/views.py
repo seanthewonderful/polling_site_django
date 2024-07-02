62e827e5e5c15e401130699a6c379eb9f44cffb2
Sean Fagan <faganseanp@gmail.com>
2024-07-02 11:22:12 -0600

    moving on to views

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
   return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+  return HttpResponse("You're looking at question %s." % question_id)
+  # The `%` operator in Python is used for string formatting. It allows you to insert values into a string in a specified format. In the example you provided, `%s` is a placeholder that will be replaced by the value of `question_id` when the string is formatted.
+  
+def results(request, question_id):
+  response = "You're looking at the results of question %s."
+  return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+  return HttpResponse("You're voting on question %s." % question_id)
+
