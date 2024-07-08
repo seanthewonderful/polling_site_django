0df68717910857ed0b1bb4d355b9144a307ae62a
Sean Fagan <faganseanp@gmail.com>
2024-07-08 10:51:34 -0600

    finish basic application

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-# Register your models here.
-admin.site.register(Question)
+# To register a model with default Django form representation:
+# admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra = 3
+  
+# Or to customize how the admin form looks and works, we can use the ModelAdmin class.
+class QuestionAdmin(admin.ModelAdmin):
+  list_display = ['question_text', 'pub_date', 'was_recently_published']
+  list_filter = ['pub_date']
+  search_fields = ['question_text']
+  fieldsets = [
+    (None, {'fields': ['question_text']}),
+    ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+  ]
+  inlines = [ChoiceInline]
+  
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -2,11 +2,18 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
   question_text = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date published')
+  
+  @admin.display(
+    boolean=True,
+    ordering='pub_date',
+    description='Published recently?'
+  )
   
   def was_recently_published(self):
     now = timezone.now()
@@ -14,6 +21,7 @@ class Question(models.Model):
   
   def __str__(self):
     return self.question_text
+  
   
 class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
